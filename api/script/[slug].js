a3adb3c277999995b4fa05d46f728461c802c4ea
reki <117578469+r0creki@users.noreply.github.com>
2026-02-05 00:18:24 +0800

    Update [slug].js

--- api/script/[slug].js
+++ api/script/[slug].js
@@ -18,7 +18,7 @@ module.exports = function handler(req, res) {
 
       "aquamatrix":
         "print('[Pevolution] AquaMatrix Loaded')\n" +
-        "loadstring(game:HttpGet("https://raw.githubusercontent.com/yunus154524/scripts/refs/heads/main/anim2gui.lua"))()",
+        "https://raw.githubusercontent.com/yunus154524/scripts/refs/heads/main/anim2gui.lua",
 
       "spear-fishing":
         "print('[Pevolution] Spear Fishing Loaded')\n" +
